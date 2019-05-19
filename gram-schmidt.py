b00b6d3fb3e7caf159f68b701bd54756700b337f
James Honea <jameshonea@gmail.com>
2019-05-19 14:59:21 -0400

    better coding practices

--- gram-schmidt.py
+++ gram-schmidt.py
@@ -1,6 +1,6 @@
 import math
 
-a = [1,0,0]
+a = [1,0,-1]
 b = [1,0,2]
 c = [0,2,2]
 
@@ -11,16 +11,15 @@ li.append(b)
 li.append(c)
 
 
-v1 = li[0]
-c = 0
+for idx, vector in enumerate(li):
+    if idx == 0:
+        c = 0
+        for e in vector:
+            c += e**2
+        c = math.sqrt(c)
 
-for e in v1:
-    c += e**2
-print(c)
-
-c = math.sqrt(c)
-print(c)
-
-v1 = [x / c for x in v1]
-
-print(v1)
+        v = [x / c for x in vector]
+        print(v)
+        
+        
+    
