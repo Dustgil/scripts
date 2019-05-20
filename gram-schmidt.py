c01c6ad1ffe51708dae0245dc65fd82ede26cb9c
James Honea <jameshonea@gmail.com>
2019-05-20 02:41:02 -0400

    script for orthonormalizing real n-dimensional vectors

--- gram-schmidt.py
+++ gram-schmidt.py
@@ -1,16 +1,45 @@
 import math
 
-a = [1,0,-1]
-b = [1,0,2]
-c = [0,2,2]
+a = [1,2,0]
+b = [1,1,0]
+c = [0,0,1]
 
-li = []
+vectors = []
 
-li.append(a)
-li.append(b)
-li.append(c)
+vectors.append(a)
+vectors.append(b)
+#vectors.append(c)
+
+orth_vectors = []
+
+for vector in vectors:
+    v = vector
+    
+    for e in orth_vectors:
+        component = 0
+        for i in range(len(e)):
+            component += e[i] * v[i]
+
+        b = [x * component for x in v]
+        
+        for i in range(len(e)):
+            v[i] = v[i] - b[i]
+
+    c = 0
+    for e in v:
+        c += e**2
+    c = math.sqrt(c)
+
+    v = [x / c for x in v]
+    orth_vectors.append(v)
+        
+    
+            
+print(orth_vectors)
 
 
+
+'''
 for idx, vector in enumerate(li):
     if idx == 0:
         c = 0
@@ -21,5 +50,5 @@ for idx, vector in enumerate(li):
         v = [x / c for x in vector]
         print(v)
         
-        
+'''      
     
