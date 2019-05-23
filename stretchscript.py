99fd04c0ebae87c038df943ed7fced7af2556a21
James Honea <jameshonea@gmail.com>
2019-05-23 16:46:05 -0400

    quick script to guide me through elbow PT routine

---
+++ stretchscript.py
@@ -0,0 +1,36 @@
+import time
+
+print("start massage right")
+time.sleep(180)
+
+winsound.PlaySound('noise.wav', winsound.SND_ASYNC)
+
+print("start stretching right")
+
+for i in range(10):
+    time.sleep(21)
+    winsound.PlaySound('noise.wav', winsound.SND_AYNSC)
+
+print("start heating left")
+time.sleep(300)
+
+winsound.PlaySound('noise.wav', winsound.SND_AYNSC)
+
+print("start massage left")
+time.sleep(180)
+
+winsound.PlaySound('noise.wav', winsound.SND_AYNSC)
+
+print("start stretching left (1)")
+
+for i in range(10):
+    time.sleep(21)
+    winsound.PlaySound('noise.wav', winsound.SND_AYNSC)
+
+print("now do (2)")
+
+for i in range(10):
+    time.sleep(21)
+    winsound.PlaySound('noise.wav', winsound.SND_AYNSC)
+
+print("done!")
