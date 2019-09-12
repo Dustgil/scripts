c4ca511b69cb6f9da8796876b123bd10dd8b2812
James Honea <jameshonea@gmail.com>
2019-09-12 02:12:28 -0400

    random character selection from inclusive or exclusive list

--- smashgen.py
+++ smashgen.py
@@ -76,6 +76,9 @@ The order is left-to-right. If you don't have a character, make sure to exclude 
 69   Isabelle
 70   Incineroar
 71   Piranha Plant
+72   Joker
+73   Dragon Quest Hero
+74   Banjo + Kazooie
 '''
 
 character_list_true = [
@@ -150,11 +153,14 @@ character_list_true = [
     'King K. Rool',
     'Isabelle',
     'Incineroar',
-    'Piranha Plant'
+    'Piranha Plant',
+    'Joker',
+    'Dragon Quest Hero',
+    'Banjo and Kazooie'
     ]
 character_list = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24, \
                   25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46, \
-                  47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71]
+                  47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74]
 
 # don't touch the next three variables.
 profile_list = []
