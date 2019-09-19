497d3141d674ad6c07aa449289db616388e85a55
James Honea <45341273+jameshonea@users.noreply.github.com>
2019-09-19 02:26:38 -0400

    first commit after reading up on finite difference

---
+++ QM1DPotentialSolver.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+# specify potential function. In future will set this up as an evaluatable
+# function of x over the range. The values over the range will be represented
+# as an nxn diagonal matrix, where n is the number of steps
+
+V = 0
+
+# calculated second derivative operator (using 3-point finite differences method)
+n = 10 # number of steps
+
+a = 0
+b = 100
+delx = (b-a)/n
+
+lists = []
+
+
+# create first row
+li = []
+element = (2/((delx)**2))
+li.append(element)
+element = (-1)/((delx)**2)
+li.append(element)
+for _ in range(n-2):
+    li.append(float(0))
+
+lists.append(li)
+
+# create middle n-2 rows
+for row in range(n-2):
+    li = []
+    for e in range(n):
+        if (e-1) == row:
+            element = (2/((delx)**2))
+        elif (e-1) == (row-1) or (e-1) == (row+1):
+            element = (-1)/((delx)**2)
+        else:
+            element = 0
+        li.append(element)
+    lists.append(li)
+
+# create last row
+li = []
+for _ in range(n-2):
+    li.append(0)
+element = (-1)/((delx)**2)
+li.append(element)
+element = (2/((delx)**2))
+li.append(element)
+lists.append(li)
+
+diff = np.array(lists)
+print(diff)
+
+    
+            
+        
+
